0c0c00c03ce61872b9cdfb801b819cc3f0238140
artem-svistelnik <artem.svistelnik@gmail.com>
2021-05-05 13:25:02 +0300

    change readme

--- course_api/models.py
+++ course_api/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 
 class Course(models.Model):
-    course_title=models.CharField(max_length=255,verbose_name='Название курса')
-    start_date=models.DateField(verbose_name='Дата начала курса')
-    finish_date=models.DateField(verbose_name='Дата завершения курса')
-    lectures_count=models.PositiveIntegerField(verbose_name='Количество лекций')
+    course_title=models.CharField(max_length=255,verbose_name='Course title')
+    start_date=models.DateField(verbose_name='Course start date')
+    finish_date=models.DateField(verbose_name='Course finish date')
+    lectures_count=models.PositiveIntegerField(verbose_name='Count of lectures')
 
     def __str__(self):
         return self.course_title
 
     class Meta:
-        verbose_name='Курс'
-        verbose_name_plural='Курсы'
+        verbose_name='Course'
+        verbose_name_plural='Courses'
         ordering = ['id']
