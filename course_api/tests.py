816367d4cb253812190d92b4a53fea5e5a3e2322
artem-svistelnik <artem.svistelnik@gmail.com>
2021-05-05 14:23:17 +0300

    change tests

--- course_api/tests.py
+++ course_api/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from .models import Course
 from rest_framework.test import APITestCase
 from rest_framework import status
-
+import requests
 
 class CourseTestCases(TestCase):
     def setUp(self) -> None:
@@ -27,10 +27,21 @@ class CourseApiTest(APITestCase):
                                           finish_date='2021-05-30',
                                           lectures_count=15)
 
-    def test_course_get_request(self):
+    def test_course_list_get_request(self):
         url='/api/courses/'
         response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK,'object not found')
+
+
+    def test_course_get_request(self):
+        url='/api/courses/{}/'.format(self.course.id)
+        response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data,{"id": 1,
+                                        "course_title": "title",
+                                        "start_date": "2021-05-05",
+                                        "finish_date": "2021-05-30",
+                                        "lectures_count": 15},'object not found')
 
     def test_course_post_request(self):
         url = '/api/courses/'
@@ -40,6 +51,11 @@ class CourseApiTest(APITestCase):
                 "lectures_count": 15}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data,{"id": 2,
+                                        "course_title": "new title",
+                                        "start_date": "2021-05-04",
+                                        "finish_date": "2021-05-21",
+                                        "lectures_count": 15},'object not created')
 
     def test_course_put_request(self):
         url = '/api/courses/{}/'.format(self.course.id)
@@ -48,9 +64,20 @@ class CourseApiTest(APITestCase):
                 "finish_date": "2021-05-21",
                 "lectures_count": 20}
         response = self.client.put(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK,'object not changed')
 
     def test_course_delete_request(self):
         url = '/api/courses/{}/'.format(self.course.id)
         response = self.client.delete(url)
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT,'object not deleted')
+        self.assertNotEqual(self.course,Course.objects.all(),'object not deleted')
+
+    def test_get_request_filter(self):
+        url = '/api/courses/?start_date=05.05.2021&finish_date=30.05.2021'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK,'request error')
+
+    def test_get_request_search(self):
+        url = '/api/courses/?search=title'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK,'request error')
